9a2be1203ae771c526c4e31351d5303c63e5302c
Nataliia Salamatina <nataliia.salamatina@gmail.com>
2021-11-28 19:49:08 +0200

    final commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import user from './json/user.json';
-import Profile from './components/profile/Profile.js';
-import Statistics from './components/statistics/StatisticsList.js';
+import Profile from './components/profile/Profile.jsx';
+import Statistics from './components/statistics/StatisticsList.jsx';
 import data from './json/data.json';
 import friendList from './json/friends.json';
-import Friends from './components/friends/Friends.js';
+import Friends from './components/friends/Friends.jsx';
 import transactionInfo from './json/transactions.json';
-import TransactionHistory from './components/transactions/Transactions.js';
+import TransactionHistory from './components/transactions/Transactions.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
